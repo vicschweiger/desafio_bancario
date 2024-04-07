8a275221a38789fc74758c6075ec0f6cef530716
Victoria Schweiger <134645175+vicschweiger@users.noreply.github.com>
2024-04-07 15:29:12 -0300

    add: extra features
    verificador de cpf e nome de usuário já cadastrado e função extrato adicionada

--- desafio_bancario.py
+++ desafio_bancario.py
@@ -1,3 +1,42 @@
+from datetime import datetime
+
+data_atual = datetime.now()
+data_atual_formatada = data_atual.strftime("%Y-%m-%d %H:%M:%S")
+
+# FUNÇÃO ADICIONAR TRANSAÇÃO
+
+extrato = []
+
+def adicionar_transacao(data_atual_formatada, operação, valor_convertido):
+    global extrato
+    nova_transacao = {
+        "data": data_atual_formatada,
+        "tipo de operacao": operação,
+        "valor": valor_convertido
+    }
+    extrato.append(nova_transacao)
+
+# FUNÇÃO EXTRATO
+
+def mostra_extrato(extrato):
+
+    if len(extrato) > 0:
+        print("Essas são as últimas transações:")
+        for transacao in extrato:
+            print(f"{transacao['data']} - {transacao['tipo de operacao'].capitalize()} - {transacao['valor']}")
+
+        entrada = int(input(f"""
+        
+        Digite 0 para voltar ao menu anterior
+                            
+        """))
+                                
+        if entrada == 0:
+            menu_operacao()
+    else:
+        print("Essa conta não possui transações recentes.")
+        menu_operacao()
+
 # FUNÇÃO SACAR
 
 limite_saque = 3
@@ -5,38 +44,43 @@ limite_saque = 3
 def sacar():
     global clientes
     global limite_saque
+    global data_atual_formatada
+    operacao = "saque"
     saldo = clientes["usuario"]["saldo"]
     valor_informado = round(float(input("Digite o valor que deseja sacar:\n")), 2)
     saque_maximo = 500
 
-    # Verifica valor de saque máximo
+    # Verifica limite de saque diário
 
-    if valor_informado > saque_maximo:
-        saque_maximo_convertido = f"R${saque_maximo:.2f}".replace('.',',')
-        print(f"Não é permitido sacar acima de R${saque_maximo_convertido}.")
+    if limite_saque <= 0:
+        print("Você atingiu o limite de saques diário.")
     else:
 
-        # Verifica saldo
+    # Verifica saldo
 
         if valor_informado > saldo:
             print("Saldo Insuficiente!!!")
         else:
 
-            # Verifica limite de saque diário
+            # Verifica valor de saque máximo
 
-            if limite_saque <= 0:
-                print("Você atingiu o limite de saques diário.")
+            if valor_informado > saque_maximo:
+                saque_maximo_convertido = f"R${saque_maximo:.2f}".replace('.',',')
+                print(f"Não é permitido sacar acima de R${saque_maximo_convertido}.")
             else:
                 saldo -= valor_informado
                 valor_convertido = f"R${valor_informado:.2f}".replace('.',',')
                 saldo_convertido = f"R${saldo:.2f}".replace('.',',')
                 limite_saque -= 1
                 print(f"Você sacou R${valor_convertido}, restam {saldo_convertido}.")
+                adicionar_transacao(data_atual_formatada, operacao, valor_convertido)
 
 # FUNÇÃO DEPOSITAR
 
 def depositar():
     global clientes
+    global data_atual_formatada
+    operacao = "depósito"
     valor_informado = round(float(input("Digite o valor que deseja depositar:\n")), 2)
     
     # Verifica se valor é positivo
@@ -48,6 +92,7 @@ def depositar():
         valor_convertido = f"R${valor_informado:.2f}".replace('.',',')
         saldo_convertido = f"R${saldo:.2f}".replace('.',',')
         print(f"Seu depósito de {valor_convertido} foi efetuado, seu saldo atual é {saldo_convertido}.")
+        adicionar_transacao(data_atual_formatada, operacao, valor_convertido)
     
     else:
         print("Deposite um valor válido.")
@@ -185,6 +230,7 @@ clientes = {
         "saldo": 0,
         "nome": "",
         "idade": "",
+        "cpf": "",
         "endereco": {
             "rua": "",
             "numero": "",
@@ -192,7 +238,7 @@ clientes = {
             "cidade": "",
             "estado": ""
         },
-        "nome_de_usuario": "",
+        "nome_de_usuario": "lala",
         "senha": ""
     }
 }
@@ -217,37 +263,59 @@ def menu_criar_conta():
     elif entrada == 1:
 
         global clientes
-        nome = str(input("Digite seu nome completo:\n"))
-        idade = int(input("Digite sua idade:\n"))
+        
+        # Verifica se nome de usuário já existe
 
-        # Verifica maioridade
+        nome_de_usuario = str(input("Digite o nome de usuário:\n"))
 
-        if idade >= 18: 
-            clientes["usuario"]["nome"] = nome
-            clientes["usuario"]["idade"] = idade
-            clientes["usuario"]["endereco"]["rua"] = str(input("Digite o logradouro:\n"))
-            clientes["usuario"]["endereco"]["numero"] = int(input("Digite o número:\n"))
-            clientes["usuario"]["endereco"]["bairro"] = str(input("Digite o bairro:\n"))
-            clientes["usuario"]["endereco"]["cidade"] = str(input("Digite a cidade:\n"))
-            clientes["usuario"]["endereco"]["estado"] = str(input("Digite o estado:\n"))
-            clientes["usuario"]["nome_de_usuario"] = input("Digite o nome de usuário:\n")
+        for clientes in clientes.values():
+            if clientes.get("nome_de_usuario") == nome_de_usuario:
+                print("Nome de usuário já cadastrado.")
 
-            # Gera Id, número de conta e saldo
+            else:
+                clientes["usuario"]["nome_de_usuario"] = nome_de_usuario
 
-            numero_de_clientes = len(clientes) - 1
-            id_da_conta = numero_de_clientes + 1
-            numero_da_conta = "{:04d}".format(id_da_conta)
+                # Verifica maioridade
 
-            clientes["usuario"]["id_da_conta"] = id_da_conta
-            clientes["usuario"]["numero_da_conta"] = numero_da_conta
-            clientes["usuario"]["saldo"] = 0
-            print(verificar_forca_senha())
+                idade = int(input("Digite sua idade:\n"))
 
-            print(menu_login())
+                if idade >= 18:
+                    clientes["usuario"]["idade"] = idade
 
-        else:
-            print("Você ainda não é maior de idade!")
-            print(menu_inicial())
+                    # Verifica CPF
+
+                    cpf = str(input("Digite seu CPF, somente números:\n"))
+
+                    for clientes_info in clientes.values():
+                        if clientes.get("cpf") == cpf:
+                            print("Esse CPF já esta cadastrado.")
+                            cpf = str(input("Digite seu CPF, somente números:\n"))
+
+                        else:
+                            clientes["usuario"]["cpf"] = cpf
+                            clientes["usuario"]["nome"] = str(input("Digite seu nome completo:\n"))
+                            clientes["usuario"]["endereco"]["rua"] = str(input("Digite o logradouro:\n"))
+                            clientes["usuario"]["endereco"]["numero"] = int(input("Digite o número:\n"))
+                            clientes["usuario"]["endereco"]["bairro"] = str(input("Digite o bairro:\n"))
+                            clientes["usuario"]["endereco"]["cidade"] = str(input("Digite a cidade:\n"))
+                            clientes["usuario"]["endereco"]["estado"] = str(input("Digite o estado:\n"))
+
+                            # Gera Id, número de conta e saldo
+
+                            numero_de_clientes = len(clientes) - 1
+                            id_da_conta = numero_de_clientes + 1
+                            numero_da_conta = "{:04d}".format(id_da_conta)
+
+                            clientes["usuario"]["id_da_conta"] = id_da_conta
+                            clientes["usuario"]["numero_da_conta"] = numero_da_conta
+                            clientes["usuario"]["saldo"] = 0
+                            print(verificar_forca_senha())
+
+                            print(menu_login())
+
+                else:
+                    print("Você ainda não é maior de idade!")
+                    print(menu_inicial())
 
 
 # MENU OPERAÇÕES
@@ -264,6 +332,7 @@ def menu_operacao():
                 1. Sacar
                 2. Depositar
                 3. Consultar saldo
+                4. Extrato
                 0. Sair
 
         ##################################################
@@ -282,6 +351,8 @@ def menu_operacao():
         
         elif entrada == 3:
             consultar_saldo()
+        
+        elif entrada == 4:
+            mostra_extrato(extrato)
 
-
-print(menu_inicial())
+print(menu_operacao())
